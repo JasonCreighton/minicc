6613aa71ab82320d166d016f4f31083170d092bd
Jason Creighton <34358430+JasonCreighton@users.noreply.github.com>
2021-04-24 17:12:08 -0700

    Add a couple "no brainer" tests

--- main.ml
+++ main.ml
@@ -10,13 +10,6 @@ let () =
 	Arg.parse speclist handle_anon_arg usage_msg;
 	if !runtests then
 		Tests.run_all ()
-	else	
-		try
-			let lexbuf = Lexing.from_channel stdin in
-			while true do
-				let result = Parser.decl Lexer.token lexbuf in
-				Amd64.emit stdout result;
-				flush stdout;
-			done
-		with Lexer.Eof ->
-			exit 0
+	else
+		Lexing.from_channel stdin |> Parser.decl Lexer.token |> Amd64.emit stdout;
+		flush stdout
--- tests.ml
+++ tests.ml
@@ -1,2 +1,13 @@
+let should_not_compile fragment =
+	let prog = Printf.sprintf "int main() { %s }" fragment in
+	try
+		(* FIXME: Don't use stdout here, emit to a buffer or something *)
+		Lexing.from_string prog |> Parser.decl Lexer.token |> Amd64.emit stdout;
+		failwith "Compile error expected"
+	with Amd64.Compile_error _ -> ()
+
 let run_all () =
+	should_not_compile "2 = 2;";
+	should_not_compile "int x; y = 7;";
+
 	print_string "Tests passed.\n"
